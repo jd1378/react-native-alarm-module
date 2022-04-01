d39bd99edda1ea2a3f0415acbf6ed196b487a017
Javad Mnjd <javad.mnjd@hotmail.com>
2022-04-01 15:08:42 +0430

    fix: update packages to use new jsx transform

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['./'],
